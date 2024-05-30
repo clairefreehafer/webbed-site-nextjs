c883382618b23bb924b27b0ff49528558ecc6925
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-05-30 11:42:56 -0500

    admin layout and title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,26 +1,35 @@
 import Link from "next/link";
-import { Fragment } from "react";
-import styled from "styled-components"
+import { ElementType, Fragment } from "react";
 
-const Title = styled.h1`
-  margin: 0.5rem auto;
-`;
+type Props = {
+  pathname: string[];
+  separator?: string;
+};
 
-export default function generateTitle(pathname: string[], separator = "▽") {
+const separatorStyles = ["", "text-xs", "text-[0.5rem]", "text-[0.3rem]"];
+
+export default function Title({ pathname, separator = "▽" }: Props) {
+  let TitleTag: ElementType = "h1";
+
   return pathname.map((slug, idx) => {
     if (idx === 0) {
-      return <Title key={slug}>claire freehafer</Title>
+      return (
+        <TitleTag key={slug} className="mb-2">
+          claire freehafer
+        </TitleTag>
+      );
     } else {
+      TitleTag = `h${idx + 1}` as ElementType;
       return (
         <Fragment key={slug}>
-          <div className={`separator-${idx}`}>{separator}</div>
-          <Title as={`h${idx + 1}`}>
+          <div className={`${separatorStyles[idx]} my-1`}>{separator}</div>
+          <TitleTag className="mb-2">
             <Link href={`/${pathname.slice(1, idx + 1).join("/")}`}>
               {slug}
             </Link>
-          </Title>
+          </TitleTag>
         </Fragment>
-      )
+      );
     }
-  })
+  });
 }
---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      animation: {
+        "wiggle-box-1": "250ms infinite wiggle-box",
+        "wiggle-box-2": "250ms calc(250ms / 3) infinite wiggle-box",
+        "wiggle-box-3": "250ms calc(2 * (250ms / 3)) infinite wiggle-box",
+      },
+      backgroundImage: ({ theme }) => ({
+        rainbow: `
+          linear-gradient(
+            135deg,
+            ${theme("colors.red")},
+            ${theme("colors.orange")},
+            ${theme("colors.yellow")},
+            ${theme("colors.green")},
+            ${theme("colors.blue")},
+            ${theme("colors.indigo")},
+            ${theme("colors.violet")}
+          )`,
+      }),
+      borderRadius: {
+        animalCrossing: {
+          ui: "3rem",
+        },
+        wiggle1: "95px 14px 92px 15px / 14px 95px 16px 95px",
+        wiggle2: "14px 92px 15px 95px / 95px 16px 95px 14px",
+        wiggle3: "255px 15px 225px 15px/15px 225px 15px 255px",
+      },
+      boxShadow: {
+        animalCrossing: {
+          ui: "0 0.75rem 0.5rem -0.5rem rgba(0, 0, 0, 0.5)",
+        },
+        "8-bit-white": "6px 0 white, -6px 0 white, 0 -6px white, 0 6px white",
+      },
+      colors: {
+        red: "red",
+        orange: "orange",
+        yellow: "yellow",
+        green: "green",
+        blue: "blue",
+        indigo: "indigo",
+        violet: "violet",
+
+        limegreen: "limegreen",
+
+        animalCrossing: {
+          uiBackground: "rgb(248, 245, 223, 0.8)",
+        },
+
+        zelda: {
+          textShadow: "rgba(0, 0, 0, 0.75)",
+        },
+      },
+      keyframes: {
+        "wiggle-box": {
+          "0%, 66%": { opacity: 0 },
+          "67%, 100%": { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
