950702ab4ab8af01a5db4c4b2b9fdf23dbc14e80
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-05-30 22:29:07 -0500

    new photo form

--- src/app/admin/photos/new/form.tsx
+++ src/app/admin/photos/new/form.tsx
@@ -1,37 +1,37 @@
 "use client";
 
 import { PhotoFormState, createPhoto } from "@actions/photo";
-import AdminForm, { Input, Label } from "@components/admin/form";
-import SubmitButton from "@components/admin/submit-button";
+import AdminForm from "@components/admin/form/index";
+import Select from "@components/admin/form/select";
+import SubmitButton from "@components/admin/form/submit-button";
+import TextInput from "@components/admin/form/text-input";
+import Textarea from "@components/admin/form/textarea";
 import { Prisma } from "@prisma/client";
+import { getAlbumNames } from "@utils/prisma";
 
-const initialState: Partial<PhotoFormState<Prisma.PhotoCreateArgs["data"]>> = {};
+const initialState: Partial<PhotoFormState<Prisma.PhotoCreateArgs["data"]>> =
+  {};
 
-export default function CreatePhotoForm(
-  { children }: { children: React.ReactNode }
-) {
+type Props = {
+  albums: Prisma.PromiseReturnType<typeof getAlbumNames>;
+};
+
+export default function CreatePhotoForm({ albums }: Props) {
   return (
     <AdminForm action={createPhoto} initialState={initialState}>
-      <Label htmlFor="smugMugKey">
-        smug mug key
-      </Label>
-      <Input type="text" name="smugMugKey" id="smugMugKey" required />
+      <TextInput label="smugmug key" name="smugmugKey" required />
 
-      <Label htmlFor="xmpPath">
-        xmp path
-      </Label>
-      <Input type="text" name="xmpPath" id="xmpPath" required />
+      <TextInput label="xmp path" name="xmpPath" required />
 
-      {children}
+      <Select
+        label="ablum"
+        name="album"
+        options={albums.map(({ name }) => name)}
+      />
 
-      <Label htmlFor="altText">
-        alt text
-      </Label>
-      <Input as="textarea" name="altText" id="altText" />
-
-      {/* TODO: show table of added data */}
+      <Textarea label="alt text" name="altText" />
 
       <SubmitButton>📸 add photo 📸</SubmitButton>
     </AdminForm>
-  )
+  );
 }
--- src/app/admin/photos/new/page.tsx
+++ src/app/admin/photos/new/page.tsx
@@ -1,13 +1,8 @@
-import AlbumSelect from "@components/admin/album-select";
 import CreatePhotoForm from "./form";
 import { getAlbumNames } from "@utils/prisma";
 
 export default async function CreatePhotoPage() {
   const albums = await getAlbumNames();
 
-  return (
-    <CreatePhotoForm>
-      <AlbumSelect albums={albums} />
-    </CreatePhotoForm>
-  )
+  return <CreatePhotoForm albums={albums} />;
 }
