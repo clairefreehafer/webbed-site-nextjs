9acdb198eb57477d84de65cc0b190521e2ed2e41
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-05-30 21:21:04 -0500

    admin landing

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,26 +1,32 @@
+"use client";
+
 import { pressStart2P } from "@fonts";
 import Link from "next/link";
+import { useState } from "react";
+
+const links = ["sections", "albums", "photos", "tags", "icons"];
 
 export default function AdminPage() {
+  const [selected, setSelected] = useState<number>();
+
+  function handleHover(index: number) {
+    setSelected(index);
+  }
+
   return (
     <ul
       className={`${pressStart2P.className} mx-auto my-12 flex w-full justify-around`}
     >
-      <li>
-        <Link href="/admin/sections">sections</Link>
-      </li>
-      <li>
-        <Link href="/admin/albums">albums</Link>
-      </li>
-      <li>
-        <Link href="/admin/photos">photos</Link>
-      </li>
-      <li>
-        <Link href="/admin/tags">tags</Link>
-      </li>
-      <li>
-        <Link href="/admin/icons">icons</Link>
-      </li>
+      {links.map((link, idx) => (
+        <li>
+          <span className={idx === selected ? "opacity-1" : "opacity-0"}>
+            ▶&nbsp;
+          </span>
+          <Link href={`/admin/${link}`} onMouseOver={() => handleHover(idx)}>
+            {link}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
