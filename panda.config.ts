38b921458dd9b80b01821a6c16cd6b84bed1cbaf
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-09-07 23:11:34 -0400

    pandacss set up

---
+++ panda.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // default theme
+  theme: {
+    tokens: {
+      gradients: {
+        rainbow: {
+          value:
+            "linear-gradient(135deg, red, orange, yellow, green, blue, indigo, violet)",
+        },
+      },
+    },
+  },
+
+  themes: {
+    admin: {},
+    animalCrossing: {},
+    book: {},
+    notebook: {},
+    zelda: {},
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+
+  // remove the default design tokens
+  presets: [],
+});
