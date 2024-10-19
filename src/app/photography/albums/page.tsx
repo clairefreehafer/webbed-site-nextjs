021c184c2193da429560464bd05f090d1b3bdf19
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-10-18 20:01:24 -0400

    just use smugmug for albums

--- src/app/photography/albums/page.tsx
+++ src/app/photography/albums/page.tsx
@@ -1,9 +1,65 @@
-import PolaroidGrid from "@components/photography/PolaroidGrid";
-import { getPolaroidGridData } from "@utils/prisma/photo";
+import PolaroidGrid, {
+  PolaroidGridAlbum,
+} from "@components/photography/PolaroidGrid";
+import { SMUGMUG_API_KEY, generateSmugMugApiUrl } from "@utils/smugmug";
+
+const SMUGMUG_KEY = "p2XgBc"; // root node
 
 export default async function Explore() {
-  // TODO: sort by date
-  const albums = await getPolaroidGridData("albums");
+  const url = generateSmugMugApiUrl("node", SMUGMUG_KEY, "children");
+  const params = new URLSearchParams({
+    APIKey: SMUGMUG_API_KEY as string,
+    _verbose: "",
+    count: "1000",
+    _config: JSON.stringify({
+      expand: {
+        HighlightImage: {
+          filter: ["ImageKey"],
+          filteruri: ["ImageSizeDetails"],
+          expand: {
+            ImageSizeDetails: {
+              filter: ["ImageUrlTemplate"],
+            },
+          },
+        },
+      },
+    }),
+  });
 
-  return <PolaroidGrid albums={albums} />;
+  console.log(`🌐 fetching ${url + params}...`);
+
+  const res = await fetch(url + params, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    return <>Error fetching smugmug data.</>;
+  }
+
+  const json = await res.json();
+  const albums = json.Response.Node;
+  const coverPhotos = json.Expansions;
+
+  const polaroidAlbums: PolaroidGridAlbum[] = albums.map((album, idx) => {
+    const highlightImageUri = album.Uris.HighlightImage.Uri;
+    const highlightImage = coverPhotos[highlightImageUri];
+    const highlightImageSizeUri =
+      highlightImage.Image?.Uris.ImageSizeDetails.Uri;
+    const coverPhotoUrl =
+      coverPhotos[highlightImageSizeUri]?.ImageSizeDetails.ImageUrlTemplate;
+
+    return {
+      id: idx,
+      name: album.Name,
+      coverPhoto: {
+        url: coverPhotoUrl,
+      },
+      randomCoverPhoto: null,
+      icon: null,
+    };
+  });
+
+  return <PolaroidGrid albums={polaroidAlbums} />;
 }
