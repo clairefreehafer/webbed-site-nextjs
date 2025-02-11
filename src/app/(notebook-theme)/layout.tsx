24877ebb490ca48f9556896ea34cb31c010b430a
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-02-11 12:29:20 -0500

    notebook theme

---
+++ src/app/(notebook-theme)/layout.tsx
@@ -0,0 +1,84 @@
+import "@/sass/notebook/style.scss";
+import localFont from "next/font/local";
+
+const loveYaLikeASister = localFont({
+  variable: "--font-love-ya-like-a-sister",
+  src: [
+    {
+      path: "../../../public/fonts/notebook/love-ya-like-a-sister.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  fallback: ["serif"],
+});
+
+const cutiveMono = localFont({
+  variable: "--font-cutive-mono",
+  src: [
+    {
+      path: "../../../public/fonts/notebook/cutive-mono.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  fallback: ["monospace"],
+});
+
+const pangolin = localFont({
+  variable: "--font-pangolin",
+  src: [
+    {
+      path: "../../../public/fonts/notebook/pangolin.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  fallback: ["system-ui", "sans-serif"],
+});
+
+const NAV_LINKS = [
+  {
+    title: "home",
+    path: "/",
+  },
+  {
+    title: "art",
+    path: "/art",
+  },
+  {
+    title: "recipes",
+    path: "/recipes",
+  },
+  {
+    title: "lists",
+    path: "/lists",
+  },
+];
+
+export default function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <div
+      className={`container ${loveYaLikeASister.variable} ${cutiveMono.variable} ${pangolin.variable}`}
+    >
+      <h1 className="name">claire freehafer</h1>
+      <nav>
+        <ul className="nav-list">
+          {NAV_LINKS.map((link) => (
+            <li className="nav-list_item" key={link.title}>
+              <div className="wiggle-box-1"></div>
+              <div className="wiggle-box-2"></div>
+              <div className="wiggle-box-3"></div>
+              <div className="wiggle-box-4"></div>
+              <div className="wiggle-box-5"></div>
+              <div className="wiggle-box-6"></div>
+              <a href={link.path}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <main className="paper">{children}</main>
+    </div>
+  );
+}
