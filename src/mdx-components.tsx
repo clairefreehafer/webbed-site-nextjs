91800885aacfaedcbbf6ca5138ed8182eceb8d20
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-07 12:09:05 -0400

    more lists

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,14 @@
 import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) => {
+      if (props.href.startsWith("http")) {
+        return <a {...props} target="_blank" />;
+      }
+      return <Link {...props} />;
+    },
     ...components,
-  }
+  };
 }
