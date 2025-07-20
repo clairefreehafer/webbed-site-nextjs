9a867784c8120fda5dec11e05792ee34ab6be327
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-20 14:32:36 -0400

    automatic puzzle colors

--- src/app/the-witness/page.tsx
+++ src/app/the-witness/page.tsx
@@ -2,7 +2,10 @@ import PuzzleBorder from "@/components/the-witness/puzzle-border";
 import { getAlbumImages } from "@/utils/digikam";
 
 export default async function Page() {
-  const images = await getAlbumImages("the witness", 650);
+  const images = await getAlbumImages("the witness", {
+    resize: 650,
+    generatePalette: true,
+  });
   return images.map((image) => (
     <PuzzleBorder key={image.filename} image={image} />
   ));
