55c29d77cc17fc2ddbcda9a998c89bf1b11c3266
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-21 12:20:43 -0400

    grid styling

--- src/app/(notebook-theme)/uncharted-4/page.tsx
+++ src/app/(notebook-theme)/uncharted-4/page.tsx
@@ -1,23 +1,23 @@
 import ImageBorder from "@/components/notebook/image-border";
-import ImageGrid from "@/components/photography/image-grid";
 import { getAlbumImages } from "@/utils/digikam";
 import Image from "next/image";
 
 export default async function Page() {
   const images = await getAlbumImages("uncharted 4");
-  const image = images[0];
 
   return (
-    <ImageBorder>
-      <Image
-        src={image.src}
-        width={image.width}
-        height={image.height}
-        className="image"
-        alt=""
-      />
-    </ImageBorder>
+    <div className="image-grid">
+      {images.map((image) => (
+        <ImageBorder key={image.filename}>
+          <Image
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            className="image"
+            alt=""
+          />
+        </ImageBorder>
+      ))}
+    </div>
   );
-
-  return <ImageGrid images={images} />;
 }
