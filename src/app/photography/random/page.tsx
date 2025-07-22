3d86fab472d79ddf0f75df0886c942301f77cc3d
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-22 12:57:39 -0400

    random image

--- src/app/photography/random/page.tsx
+++ src/app/photography/random/page.tsx
@@ -1,3 +1,9 @@
+import fg from "fast-glob";
+import RandomImage from "./random-image";
+
 export default async function Page() {
-  return "page under construction";
+  const allImages = fg
+    .sync("public/out/photography/**/*")
+    .map((filePath) => filePath.slice(6));
+  return <RandomImage allImages={allImages} />;
 }
