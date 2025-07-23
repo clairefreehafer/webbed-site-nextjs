c9604016e3f1e17d8b3a441b34491c6be7dcc411
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-23 10:56:04 -0400

    calculate bounds

--- src/app/photography/map/map.tsx
+++ src/app/photography/map/map.tsx
@@ -1,69 +1,64 @@
 "use client";
-import maplibregl, { Marker, Popup } from "maplibre-gl";
+import maplibregl, { LngLatBoundsLike, Marker, Popup } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef } from "react";
 import { GeoJson } from "@/utils/types";
 
-const BOUNDS_BUFFER = 18;
+const BOUNDS_BUFFER = 5;
 
 export default function Map({ mapData }: { mapData: GeoJson }) {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (mapRef.current) {
-      // const markers: Marker[] = [];
-      // let maxLat = -90;
-      // let minLat = 90;
-      // let maxLng = -180;
-      // let minLng = 80;
+      const markers: Marker[] = [];
+      let maxLat = -90;
+      let minLat = 90;
+      let maxLng = -180;
+      let minLng = 80;
 
-      // for (const image of images) {
-      //   console.log("album", image);
-      //   if (image.lat > maxLat) {
-      //     maxLat = image.lat;
-      //   } else if (image.lat < minLat) {
-      //     minLat = image.lat;
-      //   }
-      //   if (image.lng > maxLng) {
-      //     maxLng = image.lng;
-      //   } else if (image.lng < minLng) {
-      //     minLng = image.lng;
-      //   }
-      //   console.log(image);
-      //   const marker = new Marker({
-      //     color: image.markerColor,
-      //     draggable: false,
-      //   }).setLngLat([image.lng, image.lat]);
-      //   markers.push(marker);
-      // }
+      for (const feature of mapData.features) {
+        const [lng, lat] = feature.geometry.coordinates;
+        if (lat > maxLat) {
+          maxLat = lat;
+        } else if (lat < minLat) {
+          minLat = lat;
+        }
+        if (lng > maxLng) {
+          maxLng = lng;
+        } else if (lng < minLng) {
+          minLng = lng;
+        }
 
-      // const bounds: LngLatBoundsLike = [
-      //   [minLng, minLat], // sw
-      //   [maxLng, maxLat],
-      // ]; // ne
-      // console.log(bounds);
+        const marker = new Marker({
+          color: feature.properties.markerColor,
+          draggable: false,
+        })
+          .setLngLat(feature.geometry.coordinates)
+          .setPopup(
+            new Popup().setHTML(
+              `<p>${feature.properties.name}</p><p>${feature.properties.numberOfPhotos} photos</p>`
+            )
+          );
+        markers.push(marker);
+      }
+
+      const bounds: LngLatBoundsLike = [
+        [minLng - BOUNDS_BUFFER, minLat - BOUNDS_BUFFER], // sw
+        [maxLng + BOUNDS_BUFFER, maxLat + BOUNDS_BUFFER], // ne
+      ];
 
       const map = new maplibregl.Map({
         container: mapRef.current,
         // style: "https://tiles.stadiamaps.com/styles/stamen_watercolor.json",
         style: "https://tiles.stadiamaps.com/styles/stamen_toner.json",
-        center: [0, 0], // starting position [lng, lat]
-        zoom: 1,
-        // maxZoom: 16, // needed for watercolor
-        // bounds,
+        // center: [0, 0],
+        // zoom: 1,
+        maxZoom: 16, // required if using watercolor style
+        bounds,
       });
       map.on("load", () => {
-        for (const feature of mapData.features) {
-          const marker = new Marker({
-            color: feature.properties.markerColor,
-            draggable: false,
-          })
-            .setLngLat(feature.geometry.coordinates)
-            .setPopup(
-              new Popup().setHTML(
-                `<p>${feature.properties.name}</p><p>${feature.properties.numberOfPhotos} photos</p>`
-              )
-            );
+        for (const marker of markers) {
           marker.addTo(map);
         }
       });
