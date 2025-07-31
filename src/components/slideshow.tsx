a509480503c531b4e54447b7b9d080cb9ea58489
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-07-31 10:23:06 -0400

    slideshow improvements

--- src/components/slideshow.tsx
+++ src/components/slideshow.tsx
@@ -1,6 +1,5 @@
 import { Image } from "@/utils/digikam";
 import "@/sass/slideshow.scss";
-import NextImage from "next/image";
 
 type SlideshowProps = {
   images: Image[];
@@ -10,21 +9,12 @@ export default function Slideshow({ images }: SlideshowProps) {
   return (
     <ol className="slides">
       {images.map((image, idx) => (
-        <li
-          className="slide"
-          key={image.filename}
-          id={`${idx + 1}`}
-          style={image.background ? { background: image.background } : {}}
-        >
+        <li className="slide" key={image.filename} id={`${idx + 1}`}>
           <div className="snap-point" />
-          <NextImage
-            src={image.src}
-            alt={""}
-            id={`image-${idx + 1}`}
+          <div
             className="image"
-            height={image.height}
-            width={image.width}
-            style={image.border ? { border: image.border } : {}}
+            style={{ backgroundImage: `url('${image.src}')` }}
+            role="img"
           />
           {image.title && <p className="image-title">{image.title}</p>}
         </li>
