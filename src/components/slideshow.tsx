4bc4bcc74b4298affb4ddf96682114a6cb35b456
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-08-05 10:10:18 -0400

    way better

--- src/components/slideshow.tsx
+++ src/components/slideshow.tsx
@@ -1,7 +1,14 @@
 import { Image } from "@/utils/digikam";
 import "@/sass/components/slideshow.scss";
 import NextImage from "next/image";
-import SlideshowDate from "./slideshow-date";
+
+function formatDate(date: Date) {
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const yyyy = date.getFullYear();
+
+  return `${mm}/${dd}/${yyyy}`;
+}
 
 type SlideshowProps = {
   images: Image[];
@@ -13,11 +20,23 @@ export default function Slideshow({ images }: SlideshowProps) {
       {images.map((image, idx) => (
         <li className="slide" key={image.filename} id={`${idx + 1}`}>
           <div className="snap-point" />
-          <div
-            className="image"
-            style={{ backgroundImage: `url('${image.src}')` }}
-          >
-            {image.showDate && <SlideshowDate date={image.dateTaken} />}
+          <div className="image">
+            <NextImage
+              src={image.src}
+              fill
+              alt=""
+              style={{ objectFit: "contain" }}
+            />
+            {image.showDate && (
+              <div
+                className="photo-date"
+                style={{
+                  aspectRatio: `${image.width} / ${image.height}`,
+                }}
+              >
+                {formatDate(new Date(image.dateTaken))}
+              </div>
+            )}
           </div>
           {image.title && <p className="image-title">{image.title}</p>}
         </li>
