4054c3f8c6ff3a2b1fcf9e2e4f9b25b915c35ff0
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-08-06 12:49:53 -0400

    future home of album description

--- src/components/slideshow.tsx
+++ src/components/slideshow.tsx
@@ -6,13 +6,24 @@ import { Image } from "@/utils/digikam";
 
 import SlideshowDate from "./slideshow-date";
 
-type SlideshowProps = {
+export default function Slideshow({
+  images,
+  albumDescription,
+}: {
   images: Image[];
-};
-
-export default function Slideshow({ images }: SlideshowProps) {
+  albumDescription?: string;
+}) {
   return (
     <ol className="slides">
+      {albumDescription && (
+        <li className="slide">
+          <div className="snap-point" />
+          <p>{albumDescription}</p>
+          <p>{images.length} images</p>
+          <p className="swipe-to-view">swipe to view &rarr;</p>
+          <p className="scroll-to-view">scroll to view &rarr;</p>
+        </li>
+      )}
       {images.map((image, idx) => (
         <li className="slide" key={image.filename} id={`${idx + 1}`}>
           <div className="snap-point" />
