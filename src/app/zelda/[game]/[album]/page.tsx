b610a7448742eee629544963d229e2afd9de800c
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-08-14 16:38:33 -0400

    zelda

--- src/app/zelda/[game]/[album]/page.tsx
+++ src/app/zelda/[game]/[album]/page.tsx
@@ -1,17 +1,16 @@
 import Slideshow from "@/components/slideshow";
 import { getAlbumImages, getAlbums } from "@/utils/digikam";
 
-const GAMES = ["breath-of-the-wild", "tears-of-the kingdom"] as const;
-
 export async function generateStaticParams() {
   const pages = [];
+  const games = await getAlbums("zelda");
 
-  for (const game of GAMES) {
-    const albums = await getAlbums(game);
+  for (const game of games) {
+    const albums = await getAlbums(game.slug);
 
     for (const album of albums) {
-      console.log(`⚔️ generating /zelda/${game}/${album.slug}`);
-      pages.push({ game, album: album.slug });
+      console.log(`⚔️ generating /zelda/${game.slug}/${album.slug}`);
+      pages.push({ game: game.slug, album: album.slug });
     }
   }
 
@@ -21,10 +20,10 @@ export async function generateStaticParams() {
 export default async function Page({
   params,
 }: {
-  params: Promise<{ game: (typeof GAMES)[number]; album: string }>;
+  params: Promise<{ game: string; album: string }>;
 }) {
-  const { album } = await params;
-  const images = await getAlbumImages(album);
+  const { album, game } = await params;
+  const images = await getAlbumImages(album, game);
 
   return <Slideshow images={images} />;
 }
