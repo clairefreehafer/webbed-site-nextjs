8450b9a76d0e79e348b3b4095235e06ad3f30a10
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-08-25 15:33:03 -0400

    ingredient categories

--- src/app/(default-theme)/(with-header)/recipes/ingredients/page.tsx
+++ src/app/(default-theme)/(with-header)/recipes/ingredients/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
+import Link from "next/link";
+import { Fragment } from "react";
 
+import recipeIngredients from "@/data/recipe-ingredients.json";
 import { generateIngredients, noRobots, slugify } from "@/utils";
 
 export const metadata: Metadata = {
@@ -8,22 +11,65 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
+  const stringifiedIngredients = JSON.stringify(recipeIngredients);
   const ingredients = await generateIngredients();
+  const uncategorizedIngredients: string[] = [];
+
+  for (const ingredient of Object.keys(ingredients)) {
+    if (!stringifiedIngredients.includes(ingredient)) {
+      uncategorizedIngredients.push(ingredient);
+    }
+  }
+
+  function renderIngredients() {
+    return recipeIngredients.map((recipeIngredient) => (
+      <Fragment key={recipeIngredient.name}>
+        <h4>{recipeIngredient.name}</h4>
+        {recipeIngredient.items.map((item) => (
+          <Fragment key={item.name}>
+            <h5>{item.name}</h5>
+            <ul
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "0 1.5rem",
+                // listStyle: "none",
+                // padding: 0,
+              }}
+            >
+              {item.items.map((ingredient) => (
+                <li key={ingredient}>
+                  <Link href={`/recipes/ingredients/${slugify(ingredient)}`}>
+                    {ingredient}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Fragment>
+        ))}
+      </Fragment>
+    ));
+  }
+
   return (
     <>
       <h3>recipe ingredients</h3>
       <section className="content">
-        <ul>
-          {Object.keys(ingredients)
-            .sort()
-            .map((ingredient) => (
-              <li key={ingredient}>
-                <a href={`/recipes/ingredients/${slugify(ingredient)}`}>
-                  {ingredient}
-                </a>
-              </li>
-            ))}
-        </ul>
+        {renderIngredients()}
+        {uncategorizedIngredients.length > 0 && (
+          <>
+            <h4>uncategorized</h4>
+            <ul>
+              {uncategorizedIngredients.map((ingredient) => (
+                <li key={ingredient}>
+                  <Link href={`/recipes/ingredients/${slugify(ingredient)}`}>
+                    {ingredient}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </section>
     </>
   );
