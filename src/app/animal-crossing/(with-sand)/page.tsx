f16039a4238174f6433d1b09f004155a73e5cf44
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2025-09-12 20:45:13 -0400

    carnitas

--- src/app/animal-crossing/(with-sand)/page.tsx
+++ src/app/animal-crossing/(with-sand)/page.tsx
@@ -1,19 +1,15 @@
 import Link from "next/link";
 
-import { deslugify } from "@/utils";
-import { getAlbums } from "@/utils/digikam";
-
-export default async function Page() {
-  const games = await getAlbums("animal-crossing");
+// TODO: SVGs for each game
+export default function Page() {
   return (
     <ul>
-      {games.map((game) => (
-        <li key={game.slug}>
-          <Link href={`/animal-crossing/${game.slug}`}>
-            {deslugify(game.slug)}
-          </Link>
-        </li>
-      ))}
+      <li>
+        <Link href="/animal-crossing/new-horizons">new horizons</Link>
+      </li>
+      <li>
+        <Link href="/animal-crossing/new-leaf">new leaf</Link>
+      </li>
     </ul>
   );
 }
---
+++ src/app/animal-crossing/new-leaf/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import Slideshow from "@/components/slideshow";
+import { Grass } from "@/utils/animal-crossing/grass";
+import { getAlbumImages } from "@/utils/digikam";
+
+export const metadata: Metadata = {
+  title: "animal crossing new leaf",
+};
+
+export default async function Page() {
+  const images = await getAlbumImages("new-leaf", "animal-crossing");
+
+  return (
+    <>
+      <Grass shape="triangle" date={new Date()} />
+      <div className="breadcrumbs-container">
+        <div className="breadcrumbs">
+          <Link href="/animal-crossing">animal crossing</Link>
+          <span>/</span>
+          <h2>new leaf</h2>
+        </div>
+      </div>
+      <Slideshow images={images} />
+    </>
+  );
+}
