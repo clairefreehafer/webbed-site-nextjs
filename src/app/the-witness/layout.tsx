127cb670c797aa69145d2d5b173876cf1b75662f
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2026-02-07 12:53:47 -0500

    remove sass export

--- src/app/the-witness/layout.tsx
+++ src/app/the-witness/layout.tsx
@@ -4,33 +4,33 @@ import fs from "fs";
 import sharp from "sharp";
 
 import Navigation from "@/components/the-witness/nav";
-import variables from "@/sass/the-witness/export.module.scss";
 
 export default async function Layout({ children }: React.PropsWithChildren) {
-  const { numberOfVerticalBanners, verticalBannerFilePath } = variables;
-  for (let i = 1; i <= parseInt(numberOfVerticalBanners); i++) {
+  const numberOfVerticalBanners = 14;
+  const verticalBannerFilePath = "images/the-witness/vertical-banners";
+  for (let i = 1; i <= numberOfVerticalBanners; i++) {
     const fileName = i < 10 ? `0${i}` : `${i}`;
     if (
       !fs.existsSync(
-        `${process.cwd()}/public/${verticalBannerFilePath}/${fileName}.webp`
+        `${process.cwd()}/public/${verticalBannerFilePath}/${fileName}.webp`,
       )
     ) {
       // transformed file does not exist, so create it
       console.log(`🖼️ transforming background image...`);
       try {
         const backgroundImageBuffer = fs.readFileSync(
-          `${process.cwd()}/.local/${verticalBannerFilePath}/${fileName}.jpg`
+          `${process.cwd()}/.local/${verticalBannerFilePath}/${fileName}.jpg`,
         );
         await sharp(backgroundImageBuffer)
           .blur({ sigma: 20 })
           .webp()
           .toFile(
-            `${process.cwd()}/public/${verticalBannerFilePath}/${fileName}.webp`
+            `${process.cwd()}/public/${verticalBannerFilePath}/${fileName}.webp`,
           );
       } catch (error) {
         console.log(
           `❌ problem transforming background image:`,
-          (error as Error).message
+          (error as Error).message,
         );
       }
     }
