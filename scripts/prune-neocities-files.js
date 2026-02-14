a2f06e168fe6020c89830be918e2063c2ab2886e
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2026-02-14 13:20:18 -0500

    fix prune script

--- scripts/prune-neocities-files.js
+++ scripts/prune-neocities-files.js
@@ -11,26 +11,36 @@ const { NEOCITIES_USERNAME, NEOCITIES_PASSWORD } = process.env;
   const listResponseJson = await listResponse.json();
 
   // always delete entire _next folder
-  const filesToDelete = [];
+  const filesToDelete = new FormData();
 
   for (const file of listResponseJson.files) {
     if (!fs.existsSync(`./out/${file.path}`)) {
-      filesToDelete.push(file.path);
+      filesToDelete.append("filenames[]", file.path);
     }
   }
 
-  console.log(`found ${filesToDelete.length} files to prune from neocities...`);
+  const numberOfFiles = filesToDelete.getAll("filenames[]").length;
+
+  if (numberOfFiles === 0) {
+    console.log("no neocities files to prune");
+    return;
+  }
+
+  console.log(`found ${numberOfFiles} file(s) to delete...`);
 
   const deleteResponse = await fetch("https://neocities.org/api/delete", {
+    method: "POST",
     headers: {
       Authorization: `Basic ${btoa(`${NEOCITIES_USERNAME}:${NEOCITIES_PASSWORD}`)}`,
     },
-    body: JSON.stringify(filesToDelete),
+    body: filesToDelete,
   });
 
-  if (!deleteResponse.ok()) {
-    console.error(`❌ problem deleting files: ${await deleteResponse.text()}`);
+  const deleteResponseJson = await deleteResponse.json();
+
+  if (!deleteResponse.ok) {
+    console.error(`❌ problem deleting files: ${deleteResponseJson.message}`);
   } else {
-    console.log(`it worked`);
+    console.log(`✅ ${deleteResponseJson.message}`);
   }
 })();
